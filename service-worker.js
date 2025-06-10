ef20ee926fa96e4a714d317b7656eb9ff4068b39
Trong Phuoc <phuoctt@rabiloo.com>
2025-06-10 16:32:32 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,10 +2,7 @@ const CACHE_NAME = 'calendar-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/style.css',
-  '/script.js',
   '/manifest.json',
-  '/icons/icon-192.png',
   '/icons/icon-512.png'
 ];
 
